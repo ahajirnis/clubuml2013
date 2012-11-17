b1561016c6565b0e0c757ab4a5af684b0cfed5dc
sshaikh <sshaikh@1713bf61-df10-464b-b7f2-358ebd5c7aaf>
2012-11-17 15:35:28 +0000

    - Complete compare algorithm - Unit tested with Test.java - Contains all the supporting libraries and diagrams

---
+++ trunk/Compare/src/edu/neu/csye/clubuml2012/compareAlgorithm/ECorePackage.java
@@ -0,0 +1,43 @@
+package edu.neu.csye.clubuml2012.compareAlgorithm;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+/**
+ * Class for EPackages
+ * @author Shoeb Shaikh
+ *
+ */
+public class ECorePackage {
+	private EPackage ePackage;
+	private String name;
+
+	public ECorePackage(EList<EObject> diagram) {
+		if (diagram.get(0) instanceof EPackage) {
+			ePackage = (EPackage)diagram.get(0);
+			name = ePackage.getName().toString();
+		}
+	}
+	
+	
+	public EPackage getePackage() {
+		return ePackage;
+	}
+
+
+	public void setePackage(EPackage ePackage) {
+		this.ePackage = ePackage;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void writeStatement() {
+		// Write to report
+	}
+}
