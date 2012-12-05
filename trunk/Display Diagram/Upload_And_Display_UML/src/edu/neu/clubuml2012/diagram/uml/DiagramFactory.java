a70c0ee643b1113dc6b52067b25af5251d72c48f
kai_deng <kai_deng@1713bf61-df10-464b-b7f2-358ebd5c7aaf>
2012-12-05 04:08:15 +0000

    Betty improved DiagramFactory.java

--- trunk/Display Diagram/Upload_And_Display_UML/src/edu/neu/clubuml2012/diagram/uml/DiagramFactory.java
+++ trunk/Display Diagram/Upload_And_Display_UML/src/edu/neu/clubuml2012/diagram/uml/DiagramFactory.java
@@ -2,18 +2,23 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.neu.clubuml2012.diagram.uml;
+//package neu.kai.uml;
+
+
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.ENamedElement;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
 
 /**
@@ -24,22 +29,42 @@ public class DiagramFactory {
     
     private static String absolutePath;
     private static String libPath;
-    private static String ecoreFileName;
-    private static String javaFileName;
-    private static String dotFileName;
-    private static String pngFileName;
-    private ArrayList<EObject> pkgs;    
+    private String ecoreFileName;
+    private String javaFileName;
+    private String dotFileName;
+    private String pngFileName;
+    private ArrayList<EObject> pkgs;  
     private BufferedWriter out;
     String command;
+    File javaFile;
+    private String FontColor[] = {"red", "black", "blue", "red", "yellow" , "black"};
+    private String FillColor[] = {"pink", "orange", "LightYellow", "PaleGreen", "White", "LightGray"};
+    private int pkgIndex = 0;
+    private final int MAX_NUM_FONT_COLORS = 6;
     
-    
+    public static void main(String[] args) {
+        
+        //parse args
+        if (args.length <=2)
+        {
+            System.out.println("usage:  CreatePng PathAndNameToEcorefile newname pathToLib");
+            System.out.println("        where PathAndNameToEcorefile is the ecore source file");
+            System.out.println("        and newname is the new file name");
+            System.out.println("        and pathToLib is the UMLGRAPH library");
+            return;
+        }
+        
+
+        DiagramFactory obj = new DiagramFactory(args[0],args[1], args[2]);
+        obj.process();
+    }
     public DiagramFactory(String absolutePath, String newName, String libPath){
         DiagramFactory.absolutePath = absolutePath;
         DiagramFactory.libPath = libPath;
         ecoreFileName = newName;
-        javaFileName = newName + ".java";
+        javaFileName = newName  + ".java";
         dotFileName = newName + ".dot";
-        pngFileName = newName + ".png";
+        pngFileName = newName + ".png"; 
     }
 
     public String process() {
@@ -62,134 +87,31 @@ public class DiagramFactory {
 
         //Create Stream Writer
         try {
+            javaFile = new File(absolutePath + javaFileName);
+            FileWriter fstream = new FileWriter(javaFile);
+            out = new BufferedWriter(fstream);
+            
+            // write a header for the whole file
+            out.write("import java.util.Date;\n");
+            out.write("import java.util.*; \n");
+            out.write("import java.io.*; \n");
+            
+            out.write("\n");
+            out.write("/**\n");
+            out.write(" * @opt edgecolor \"yellow\"\n");
+            out.write(" * @opt nodefontcolor \"red\"\n");
+            out.write(" * @opt nodefillcolor \"#a0a0a0\"\n");
+            out.write(" * @hidden\n");
+            out.write(" */ \n");
+            
+            pkgIndex = 0;
+            
             //for each package
             for (int k = 0; k < pkgs.size(); k++) {
-                //name java file after first class in pkg  
-                File javaFile = new File(absolutePath + javaFileName);
-                FileWriter fstream = new FileWriter(javaFile);
-                out = new BufferedWriter(fstream);
-
-                out.write("//Name of Ecore Package = " + pkgs.get(k) + "\n\n");
-                out.write("import java.util.Date;\n");
-                out.write("import java.util.*; \n");
-                out.write("import java.io.*; \n");
-
-                boolean firstClass = true;
-                //iterate thru an array list of classes 
-                EList<EObject> clsList = pkgs.get(0).eContents();
-                for (int i = 0; i < clsList.size(); i++) {
-                    EClass cls = (EClass) clsList.get(i);
-
-                    out.write("\n");
-                    out.write("/**\n");
-                    //this is where we would write what this contains
-                    EList<EReference>  refList = cls.getEReferences();
-                    for (int j = 0; j < refList.size(); j++) {
-                       System.out.println(cls + "\n        "+ refList.get(j)) ;
-                       EReference ref = refList.get(j);
-                       //if (ref.isContainer()) {  // didn't print anything
-                       if (ref.isContainment()) {   
-                           out.write (" * @composed 1 ");
-
-                           if (ref.getName() == null) {
-                               out.write(" - ");  
-                           } else {
-                               out.write(" ");
-                               out.write(ref.getName());
-                               out.write(" ");
-                           }
-                           //This is the wrong upper bound, I need the bound at the other end
-                           if (ref.getLowerBound() == 0){
-                               out.write("0");
-                           } else {
-                               out.write(ref.getLowerBound());
-                           }
-                           //if there is a range of values
-                           if (ref.getLowerBound() != ref.getUpperBound()) {//
-                               out.write("..");
-                               if (ref.getUpperBound() == -1) {
-                                   out.write ("*");
-                               } else {
-                                   out.write(ref.getUpperBound());
-                               }
-                           }
-                           out.write( " " + ref.getEReferenceType().getName() + "\n");   
-                           System.out.println(ref.getName());   
-                           
-                       } else {
-                           out.write (" * @has 1 ");
-                           if (ref.getLowerBound() == -1) {
-                              out.write ("1..*");
-                           } else if (ref.getLowerBound() == 0){
-                               out.write("0");
-                           } else {
-                               out.write(ref.getLowerBound());
-                          }
-                           //if (ref.isUnique())
-                           if (ref.getName() == null) {
-                               out.write(" - ");  
-                           } else {
-                               out.write(" ");
-                               out.write(ref.getName());
-                               out.write(" ");
-                           }
-                           //This is the wrong upper bound, I need the bound at the other end
-                           if (ref.getLowerBound() == 0){
-                               out.write("0");
-                           } else if (ref.getLowerBound() == 1){
-                               out.write("1");
-                           } else {
-                               out.write(ref.getLowerBound());
-                               //System.out.println (ref.getLowerBound());
-                           }
-                           System.out.println (ref.getLowerBound());
-                           System.out.println (ref.getUpperBound());
-                           //if there is a range of values
-                           if (ref.getLowerBound() != ref.getUpperBound()) {//
-                               out.write("..");
-                               if (ref.getUpperBound() == -1) {
-                                   out.write ("*");
-                               } else {
-                                   out.write(ref.getUpperBound());
-                               }
-                           }
-                           out.write( " " + ref.getEReferenceType().getName() + "\n");               
-                       }
-                    }
-                    out.write(" */ \n");
-
-                    if (firstClass) {
-                        //only first one is public: otherwise get errors in UMLGraph
-                        out.write("class " + cls.getName());
-                        firstClass = false;
-                    } else {
-                        out.write("class " + cls.getName());
-                    }
-                    //check if this extends any other classes
-                    EList<EClass> superClsList = cls.getEAllSuperTypes();
-                    for (int j = 0; j < superClsList.size(); j++) {
-                        EClass otherCls = superClsList.get(j);
-                        out.write(" extends " + otherCls.getName());
-                    }
-                    out.write(" {\n");
-
-                    //for each variable in the class
-                    EList<EAttribute> attr = cls.getEAllAttributes();
-                    for (int j = 0; j < attr.size(); j++) {
-                        out.write("    " + attr.get(j).getEAttributeType().getInstanceClassName()
-                                + " " + attr.get(j).getName() + ";\n");
-                    }
-
-                    //get operation objects into a list
-                    EList<EOperation> ope = cls.getEAllOperations();
-                    for (int j = 0; j < ope.size(); j++) {
-                        System.out.println("119");
-                        out.write("    " + ope.get(j).getEType() + " " + ope.get(j).getName() + ";\n");
-                    }
-                    out.write("}\n");
-                }
-                out.close();
+                readPkgForClasses(pkgs.get(k));          
+                
             }
+            out.close();
         } catch (Exception e) {
             try {
                 out.close();
@@ -200,32 +122,166 @@ public class DiagramFactory {
         }
     }
 
+    //This method is recursive
+    public void readPkgForClasses(EObject obj) {
+        try {
+            //determine if this is a pkg
+            if (obj instanceof EPackage) {
+                // get contents of pkg
+                //increment pkgIndex for colors
+                pkgIndex = (pkgIndex + 1) % MAX_NUM_FONT_COLORS;
+                out.write("//Name of Ecore Package = " + ((ENamedElement) obj).getName()  + "\n\n");
+                EList<EObject> objList = obj.eContents();
+               for (int k = 0; k < objList.size(); k++) {
+                   readPkgForClasses(objList.get(k));
+
+                }
+             } else {       //if not a pkg must be a class since underneath a package  
+                EClass cls = (EClass) obj;
+                out.write("\n");
+                out.write("/**\n");
+                //define colors for the class
+                out.write(" * @opt nodefontcolor " + "\"" + FontColor[pkgIndex] + "\""  + "\n");
+                out.write(" * @opt nodefillcolor " + "\"" + FillColor[pkgIndex] + "\""  + "\n");              
+                //this is where we would write what this contains
+                EList<EReference>  refList = cls.getEReferences();
+                for (int j = 0; j < refList.size(); j++) {
+                   EReference ref = refList.get(j);
+                   
+                   if (ref.isContainment()) {   
+                       out.write (" * @composed 1 ");
+
+                       if (ref.getName() == null) {
+                           out.write(" - ");  
+                       } else {
+                           out.write(" ");
+                           out.write(ref.getName());
+                           out.write(" ");
+                       }
+                       //This is the wrong upper bound, I need the bound at the other end
+                       if (ref.getLowerBound() == 0){
+                           out.write("0");
+                       } else {
+                           out.write(ref.getLowerBound());
+                       }
+                       //if there is a range of values
+                       if (ref.getLowerBound() != ref.getUpperBound()) {//
+                           out.write("..");
+                           if (ref.getUpperBound() == -1) {
+                               out.write ("*");
+                           } else {
+                               out.write(ref.getUpperBound());
+                           }
+                       }
+                       out.write( " " + ref.getEReferenceType().getName() + "\n");   
+                   
+                   } else {
+                       out.write (" * @has ");
+                       if (ref.getLowerBound() == -1) {
+                           out.write ("1..*");
+                       } else if (ref.getLowerBound() == 0){
+                           out.write("0");
+                       } else if (ref.getLowerBound() == 1){
+                           out.write("1");
+                       } else {
+                           out.write(ref.getLowerBound());
+                       }
+                       if (ref.getName() == null) {
+                           out.write(" - ");  
+                       } else {
+                           out.write(" ");
+                           out.write("\"" + ref.getName() + "\"");
+                           out.write(" ");
+                       }
+                       //This might be the wrong upper bound, I need the bound at the other end
+                       if (ref.getLowerBound() == 0){
+                           out.write("0");
+                       } else if (ref.getLowerBound() == 1){
+                           out.write("1");
+                       } else {
+                           out.write(ref.getLowerBound());
+                       }
+                       //if there is a range of values
+                       if (ref.getLowerBound() != ref.getUpperBound()) {//
+                           out.write("..");
+                           if (ref.getUpperBound() == -1) {
+                               out.write ("*");
+                           } else {
+                               out.write(ref.getUpperBound());
+                           }
+                       }
+                       out.write( " " + ref.getEReferenceType().getName() + "\n");               
+                   }
+                }
+                out.write(" */ \n");
+
+                out.write("class " + cls.getName());
+
+                //check if this extends any other classes
+                EList<EClass> superClsList = cls.getEAllSuperTypes();
+                for (int j = 0; j < superClsList.size(); j++) {
+                    EClass otherCls = superClsList.get(j);
+                    out.write(" extends " + otherCls.getName());
+                }
+                out.write(" {\n");
+
+                //for each variable in the class
+                EList<EAttribute> attr = cls.getEAllAttributes();
+                for (int j = 0; j < attr.size(); j++) {
+                    out.write("    " + attr.get(j).getEAttributeType().getInstanceClassName()
+                            + " " + attr.get(j).getName() + ";\n");
+                }
+
+                //get operation objects into a list
+                EList<EOperation> ope = cls.getEAllOperations();
+                for (int j = 0; j < ope.size(); j++) {
+                    out.write("    " + ope.get(j).getEType().getInstanceTypeName()     + " " + ope.get(j).getName() + ";\n");
+                }
+                out.write("}\n");
+            }
+       // } 
+        } catch (Exception e) {
+            System.out.println("Got an writing to file" + e); 
+        }
+    }
+    
+    
     public void createPngFile() {
         try {
-            //String command1 = "java -jar C:\\java\\Project\\UMLGraph-5.6\\lib\\UmlGraph.jar -all -private " + javaFileName + ".java";
-            
-            //String toolPath = "/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/lib/tools.jar";
-            String umlGraphPath = libPath + "UmlGraph-5.6.jar";
-            String command1[] = {"java", "-jar", umlGraphPath, "-all", "-private", absolutePath + javaFileName, 
-            		"-output", absolutePath + dotFileName};
+            String umlGraphPath = libPath + "UmlGraph.jar";
+            String command1 = "java" + " -jar " + umlGraphPath + " -all -private "+ absolutePath + javaFileName +
+                    " -output " +  absolutePath + dotFileName;
             Runtime.getRuntime().exec(command1);
             try {
+                // have to wait before go on, so that the shell has time to create the dot file
                 Thread.sleep(1000L);
             } catch (Exception e) {
             }
             
             //Graphviz_Path: C:\Program Files (x86)\Graphviz 2.28\bin
             String command2[] = {"dot", "-Tpng", "-o", absolutePath + pngFileName,
-            		absolutePath + dotFileName};
+                    absolutePath + dotFileName};
             Runtime.getRuntime().exec(command2);
             command = command2.toString();
             try {
-                Thread.sleep(500L);
+                Thread.sleep(2000L);
             } catch (Exception e) {
             }
         } catch (Exception e) {
             System.out.println("Error in creating the png file");
             e.printStackTrace();
         }
+    }
+    
+    public static void copyFile( File from, File to ) throws IOException {
+        
+        // Attempt to delete it
+        if(to.exists()){
+            boolean success = to.delete();
+            if (!success) {
+                throw new IllegalArgumentException("Delete: deletion failed");      
+            }
+         }
+        Files.copy( from.toPath(), to.toPath() );
     }
 }//end class
