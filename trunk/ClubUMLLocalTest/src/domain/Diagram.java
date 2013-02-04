803141a39abf39007ea04b3abb83a331aa578282
guo.do <guo.do@1713bf61-df10-464b-b7f2-358ebd5c7aaf>
2013-02-04 17:46:28 +0000

    documentation of domain package

--- trunk/ClubUMLLocalTest/src/domain/Diagram.java
+++ trunk/ClubUMLLocalTest/src/domain/Diagram.java
@@ -1,5 +1,13 @@
 package domain;
 
+/**
+ * Information class that contain all the features of one Diagram
+ * @ doc author	Dong Guo
+ * 
+ * not finished: inEdition. line: 31; 110-117
+ * 				 projectId. line: 150
+ */
+
 public class Diagram {
 
     private int diagramId;
@@ -9,11 +17,26 @@ public class Diagram {
     private int ownerId;
     private int projectId = 2; // default;
     private String ecoreFilePath;
+    
 
-    public Diagram()
-    {}
-
-    public Diagram(int diagramId, String diagramName, String createdTime, boolean inEdition, int ownerId, String ecoreFilePath) {
+	/**
+	 * Constructor to initialize necessary class members
+	 *
+	 * @param diagramId
+	 * 			The ID of the diagram
+	 * @param diagramName
+	 * 			The name of the diagram
+	 * @param createdTime
+	 * 			The time that this diagram was created
+	 * @param inEdition
+	 * 			
+	 * @param ownerId
+	 * 			The ID of the owner of this diagram
+	 * @param ecoreFilePath
+	 * 			The filepath of this diagram
+	 */
+    public Diagram(int diagramId, String diagramName, String createdTime, boolean inEdition,
+    		int ownerId, String ecoreFilePath) {
         this.diagramId = diagramId;
         this.diagramName = diagramName;
         this.createdTime = createdTime;
@@ -22,32 +45,70 @@ public class Diagram {
         this.ecoreFilePath = ecoreFilePath;
     }
 
-
-
-    public void setCreatedTime(String createdTime) {
-        this.createdTime = createdTime;
+	/**
+	 * Default constructor
+	 */
+    public Diagram(){
     }
 
+	/**
+	 * Gete the createdTime
+	 * 
+	 * @return createdTime String
+	 */
     public String getCreatedTime() {
         return createdTime;
     }
-
+    
+	/**
+	 * Set createdTime
+	 * 
+	 * @param createdTime
+	 * 			The time that this diagram was created
+	 */
+    public void setCreatedTime(String createdTime) {
+        this.createdTime = createdTime;
+    }
+    
+	/**
+	 * Gete the diagramId
+	 * 
+	 * @return diagramId int
+	 */
     public int getDiagramId() {
         return diagramId;
     }
 
+	/**
+	 * Set createdTime
+	 * 
+	 * @param diagramId
+	 * 			The ID of this diagram
+	 */
     public void setDiagramId(int diagramId) {
         this.diagramId = diagramId;
     }
-
+    
+	/**
+	 * Gete the diagramName
+	 * 
+	 * @return diagramName String
+	 */
     public String getDiagramName() {
         return diagramName;
     }
 
+	/**
+	 * Set diagramName
+	 * 
+	 * @param diagramName
+	 * 			The name of this diagram
+	 */    
     public void setDiagramName(String diagramName) {
         this.diagramName = diagramName;
     }
 
+    
     public boolean isInEdition() {
         return inEdition;
     }
@@ -56,29 +117,66 @@ public class Diagram {
         this.inEdition = inEdition;
     }
 
+	/**
+	 * Gete the ownerId
+	 * 
+	 * @return ownerId int
+	 */
     public int getOwnerId() {
         return ownerId;
     }
 
+	/**
+	 * Set ownerId
+	 * 
+	 * @param ownerId
+	 * 			The ID of the owner of this diagram
+	 */
     public void setOwnerId(int ownerId) {
         this.ownerId = ownerId;
     }
 
+	/**
+	 * Gete the projectId
+	 * 
+	 * @return projectId int
+	 */
     public int getProjectId() {
         return projectId;
     }
 
+	/**
+	 * Set projectId
+	 * 
+	 * @param projectId
+	 * 			
+	 */
     public void setProjectId(int projectId) {
         this.projectId = projectId;
     }
 
+	/**
+	 * Gete the ecoreFilePath
+	 * 
+	 * @return ecoreFilePath String
+	 */
     public String getEcoreFilePath() {
         return ecoreFilePath;
     }
-
+    
+	/**
+	 * Set ecoreFilePath
+	 * 
+	 * @param ecoreFilePath
+	 * 			The filepath of this diagram
+	 */
     public void setEcoreFilePath(String ecoreFilePath) {
         this.ecoreFilePath = ecoreFilePath;
     }
+    
+	/**
+	 * Some functions that have not been used yet
+	 */
 //	public String getImageFilePath() {
 //		return imageFilePath;
 //	}
--- trunk/ClubUMLLocalTest/src/domain/Report.java
+++ trunk/ClubUMLLocalTest/src/domain/Report.java
@@ -1,5 +1,10 @@
 package domain;
 
+/**
+ * Information class that contains all the features of one report
+ * @ doc author	Dong Guo
+ */
+
 public class Report {
 
     private int reportId;
@@ -8,42 +13,97 @@ public class Report {
     private String compareTime;
     private String reportFilePath;
 
+	/**
+	 * Gete the reportId
+	 * 
+	 * @return reportId int
+	 */
     public int getReportId() {
         return reportId;
     }
 
+	/**
+	 * Set reportId
+	 * 
+	 * @param reportId
+	 * 			The ID of the report
+	 */
     public void setReportId(int reportId) {
         this.reportId = reportId;
     }
 
+	/**
+	 * Gete the diagramA_Id
+	 * 
+	 * @return diagramA_Id int
+	 */
     public int getDiagramA_Id() {
         return diagramA_Id;
     }
 
+	/**
+	 * Set diagramA_Id
+	 * 
+	 * @param diagramA_Id
+	 * 			The ID of diagram-A
+	 */
     public void setDiagramA_Id(int diagramA_Id) {
         this.diagramA_Id = diagramA_Id;
     }
 
+	/**
+	 * Gete the diagramB_Id
+	 * 
+	 * @return diagramB_Id int
+	 */
     public int getDiagramB_Id() {
         return diagramB_Id;
     }
 
+	/**
+	 * Set diagramB_Id
+	 * 
+	 * @param diagramB_Id
+	 * 			The ID of diagram-B
+	 */
     public void setDiagramB_Id(int diagramB_Id) {
         this.diagramB_Id = diagramB_Id;
     }
 
+	/**
+	 * Gete the compareTime
+	 * 
+	 * @return compareTime String
+	 */
     public String getCompareTime() {
         return compareTime;
     }
 
+	/**
+	 * Set compareTime
+	 * 
+	 * @param compareTime
+	 * 			The time that this compare happened
+	 */
     public void setCompareTime(String compareTime) {
         this.compareTime = compareTime;
     }
 
+	/**
+	 * Gete the reportFilePath
+	 * 
+	 * @return reportFilePath String
+	 */
     public String getReportFilePath() {
         return reportFilePath;
     }
 
+	/**
+	 * Set reportFilePath
+	 * 
+	 * @param reportFilePath
+	 * 			The filepath of this report
+	 */
     public void setReportFilePath(String reportFilePath) {
         this.reportFilePath = reportFilePath;
     }
