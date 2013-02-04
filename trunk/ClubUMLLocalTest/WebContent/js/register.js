b6ea2b515941e2af972738a2f4f49eb1df1918d0
rdo524 <rdo524@1713bf61-df10-464b-b7f2-358ebd5c7aaf>
2013-02-04 05:20:53 +0000

    documentation

--- trunk/ClubUMLLocalTest/WebContent/js/register.js
+++ trunk/ClubUMLLocalTest/WebContent/js/register.js
@@ -1,14 +1,9 @@
-
-// Submit button is enabled when user name, password and email fields are valid
-var validUserName = false;
-var validPassword = false;
-var validEmail = false;
-
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Validates all required fields.
+ * 
+ * @returns true if all fields are valid, else false
  */
-function validate() {
+function validateForm() {
 	if (document.forms.register.username.value == "") {
 		alert("User Name can not be empty!");
 		return false;
@@ -17,67 +12,78 @@ function validate() {
 		return false;
 	} else if (document.forms.register.password.value != document.forms.register.password2.value) {
 		alert("Password does not match!");
-
 		return false;
 	} else if (document.forms.register.email.value == "") {
 		alert("Email can not be empty!");
 		return false;
 	}
+	
+	return true;
 }
 
+/**
+ * Check if user name already exist in user table.
+ * 
+ * @param value is the user name to check
+ */
 function checkUsername(value) {
 
 	var block = document.getElementById("username");
-	
+
 	ajax = createAjax();
 	ajax.onreadystatechange = function() {
 		if (ajax.readyState == 4 && ajax.status == 200) {
-			
-			var response = ajax.responseText;
-			
-			var patt1 = new RegExp("green");
-
-			validUserName = patt1.test(response);
-			
 			block.innerHTML = ajax.responseText;
 		}
-	}
+	};
 	ajax.open("get", "ValidateServlet?username=" + value + "", true);
 	ajax.send(null);
 }
 
+/**
+ * Validate passwords match.
+ * 
+ * Displays message for valid and invalid password.
+ */
 function checkPassword() {
 	var block2 = document.getElementById("password");
 	var password1 = document.forms.registerForm.password.value;
 	var password2 = document.forms.registerForm.password2.value;
-	
+
 	if (password1 == "" || password2 == "") {
 		block2.innerHTML = "";
-		validPassword = false;
 	} else if (password1 != password2) {
 		block2.innerHTML = "<font color = 'red'>Password does not match";
-		validPassword = false;
 	} else {
 		block2.innerHTML = "<font color = 'green'>Password ok";
-		validPassword = true;
 	}
 }
 
+/**
+ * Validate email format.
+ * 
+ * Displays message for valid and invalid email.
+ * 
+ * @param email to verify
+ */
 function checkEmail(email) {
 	var block = document.getElementById("email");
-	var patt1 = new RegExp(".+@.+\.[com|net|org|biz|gov]$");
+	var patt1 = new RegExp(".+@.+\.[com|net|org|biz|gov|edu]$");
 
-	validEmail = patt1.test(email);
-	
 	if (email == "") {
 		block.innerHTML = "";
-	} else if (validEmail) {
+	} else if (patt1.test(email)) {
 		block.innerHTML = "<font color = 'green'>Email ok";
 	} else {
 		block.innerHTML = "<font color = 'red'> Invalid email";
 	}
 }
 
+/**
+ * Creates Ajax object
+ * 
+ * @returns Ajax object
+ */
 function createAjax() {
 	var xmlhttp;
 	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -89,23 +95,13 @@ function createAjax() {
 }
 
 /**
- * checks if user name, password and email fields are valid to enable submit button
- * else submit button is disabled
+ * (Have not seen used. Candidate for removal)
+ * @returns
  */
-function ProcessSubmitState() {
-	if (validUserName && validPassword && validEmail) {
-		document.getElementById('SubmitButton').disabled = "";
-	} else {
-		document.getElementById('SubmitButton').disabled = "true";
-	}
-		
-}
-
 function checkPromote() {
 	if (document.forms.promote1.comment.value == ""
 			|| document.forms.promote1.comment.value == null) {
 		alert("Comment cannot be empty!");
 		return false;
 	}
-
 }
