be74bbc9ca1aa75fc8565149260d2105b74668c1
guo.do <guo.do@1713bf61-df10-464b-b7f2-358ebd5c7aaf>
2013-03-18 19:32:15 +0000

    SynonymCheck.java, using public API

--- trunk/ClubUMLSpring2013/src/controller/similaritycheck/SynonymCheck.java
+++ trunk/ClubUMLSpring2013/src/controller/similaritycheck/SynonymCheck.java
@@ -1,5 +1,14 @@
 package controller.similaritycheck;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Hashtable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SynonymCheck {
 	
 	/**
@@ -12,15 +21,55 @@ public class SynonymCheck {
 	 *            
 	 * @return true if the two names are synonyms for each other
 	 */
-	public static boolean isSimilarThesaurus(String name1, String name2){
-		
-		name1 = name1.toLowerCase();
-		name2 = name2.toLowerCase();
-		
-		/*
-		 *
-		 */
-		
-		return true;
-	}
+    public static boolean isSimilarThesaurus(String name1, String name2) {
+
+        String line;
+        String responseString;
+        Hashtable responseTable = new Hashtable();
+
+        BufferedReader rd;
+        OutputStreamWriter wr;
+        StringBuilder API_response = new StringBuilder();
+
+        name1 = name1.toLowerCase();
+        name2 = name2.toLowerCase();
+
+        try {
+
+            // API key: eb23eace35633b1274abdfeabc1b4753
+            URL url = new URL("http://words.bighugelabs.com/api/2/eb23eace35633b1274abdfeabc1b4753/" + name1 + "/json");
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.flush();
+
+            // Get the response
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            while ((line = rd.readLine()) != null) {
+                API_response.append(line);
+            }
+
+            //Store the response as one big string
+            responseString = API_response.toString();
+
+            //Find out the words between two quotation marks, put them into a hushatble
+            Pattern p = Pattern.compile("\"(.*?)\"");
+            Matcher m = p.matcher(responseString);
+            while (m.find()) {
+                responseTable.put(m.group(), true);
+            }
+
+            //Find out if name2 is in those keys
+            name2 = "\"" + name2 + "\"";
+            if (responseTable.containsKey(name2)) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return true;
+    }
 }
