847e87042ed6223e022d7a32a8d6cc60239e1c50
mhsong <mhsong@1713bf61-df10-464b-b7f2-358ebd5c7aaf>
2013-04-01 19:23:17 +0000

    [Update] Add request

--- trunk/ClubUMLSpring2013/src/controller/comparer/xmi/request/Add.java
+++ trunk/ClubUMLSpring2013/src/controller/comparer/xmi/request/Add.java
@@ -1,10 +1,14 @@
 package controller.comparer.xmi.request;
 
+import java.util.ArrayList;
+
 import org.json.simple.JSONObject;
 
+import controller.comparer.xmi.XmiAttributeElement;
 import controller.comparer.xmi.XmiClassDiagramComparer;
 import controller.comparer.xmi.XmiClassElement;
 import controller.comparer.xmi.Utility;
+import controller.comparer.xmi.XmiOperationElement;
 import controller.merge.xmi.xclass.XmiMergedClass;
 
 /**
@@ -36,52 +40,56 @@ public class Add implements Request {
 		String className;
 		XmiClassElement classElement;
 		
-		JSONObject obj = new JSONObject();		
+		JSONObject obj = new JSONObject();				
+		ArrayList<String> attribute = new ArrayList<String>();
+		ArrayList<String> operation = new ArrayList<String>();
+
 		try {   
 			if (jsonObj.get(CLASS_DIAGRAM_1) != null) {
 				className = (String)jsonObj.get(CLASS_DIAGRAM_1);
 			    classElement = Utility.getClassByName(comparer.getUniqueClass1(), className);
-			    obj.put(CLASS_DIAGRAM_1, className);			    
+			    obj.put(CLASS_DIAGRAM_1, className);			 		    
 			    if(classElement != null) {
-				    if(classElement.getAttributes() != null) {
-				    	obj.put(ATTRIBUTE,classElement.getAttributes());
-				    } else {
-				    	obj.put(ATTRIBUTE,null);
-				    }
-				    if(classElement.getOperations() != null) {
-				    	obj.put(OPERATION,classElement.getOperations());
-				    } else {
-				    	obj.put(OPERATION,null);
-				    }
+					for(XmiAttributeElement attr : classElement.getAttributes()){
+						attribute.add(attr.getName());
+					}
+					for(XmiOperationElement oper : classElement.getOperations()){
+						operation.add(oper.getName());
+					} 
+				    //obj.put(ATTRIBUTE,classElement.getAttributes());
+					//obj.put(OPERATION,classElement.getOperations());
+				    obj.put(ATTRIBUTE, attribute);
+				    obj.put(OPERATION, operation);
+				    obj.put(TITLE_RESPONSE,"Success");
 			    } else {
 			    	obj.put(ATTRIBUTE,null);
 			 	    obj.put(OPERATION,null);
+			 	    obj.put(TITLE_RESPONSE,"Fail");
 			    }
-			    obj.put(TITLE_RESPONSE,"Success");
 			} else if (jsonObj.get(CLASS_DIAGRAM_2) != null) {
 				className = (String)jsonObj.get(CLASS_DIAGRAM_2);
 				classElement = Utility.getClassByName(comparer.getUniqueClass2(), className);
 				obj.put(CLASS_DIAGRAM_2, className);
 				if(classElement != null) {
-				    if(classElement.getAttributes() != null) {
-				    	obj.put(ATTRIBUTE,classElement.getAttributes());
-				    } else {
-				    	obj.put(ATTRIBUTE,null);
-				    }
-				    if(classElement.getOperations() != null) {
-				    	obj.put(OPERATION,classElement.getOperations());
-				    } else {
-				    	obj.put(OPERATION,null);
-				    }
+					for(XmiAttributeElement attr : classElement.getAttributes()){
+						attribute.add(attr.getName());
+					}
+					for(XmiOperationElement oper : classElement.getOperations()){
+						operation.add(oper.getName());
+					}
+				    //obj.put(ATTRIBUTE,classElement.getAttributes());
+					//obj.put(OPERATION,classElement.getOperations());
+				    obj.put(ATTRIBUTE, attribute);
+				    obj.put(OPERATION, operation);
+				    obj.put(TITLE_RESPONSE,"Success");
 				} else {
 					obj.put(ATTRIBUTE,null);
 			 	    obj.put(OPERATION,null);
+			 	   obj.put(TITLE_RESPONSE,"Fail");
 				}
-				obj.put(TITLE_RESPONSE,"Success");
 			} else {
 				obj.put(TITLE_RESPONSE,"Fail");
 			}
-			
 		} catch (NullPointerException npe) {   
 			obj.put(TITLE_RESPONSE,"Fail");
 			System.out.println("Add request : " + npe);
