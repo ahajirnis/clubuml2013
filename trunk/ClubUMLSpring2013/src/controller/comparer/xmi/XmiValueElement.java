2d401772e3c70bd60932a554cd3e9b03dfd78fd7
rdo524 <rdo524@1713bf61-df10-464b-b7f2-358ebd5c7aaf>
2013-04-09 02:07:57 +0000

    [Update] Update constructor

--- trunk/ClubUMLSpring2013/src/controller/comparer/xmi/XmiValueElement.java
+++ trunk/ClubUMLSpring2013/src/controller/comparer/xmi/XmiValueElement.java
@@ -3,12 +3,25 @@ package controller.comparer.xmi;
 public class XmiValueElement extends XmiBaseElement
 {
 	private String value;
-
+	
+	// True if value is null
+	private boolean isNull;
+	
+	public XmiValueElement(String id, String type,
+			boolean isNull) {
+		super(id, "", type, "");
+		setNull(isNull);
+	}
+	
+	public XmiValueElement(String id, String name, String type,
+			String value) {
+		super(id, name, type, "");
+		this.setValue(value);
+	}
+	
 	public XmiValueElement(String id, String type,
 			String value) {
-		super(id, "", type, "");
-		this.setValue(value);
-		// TODO Auto-generated constructor stub
+		this(id, "", type, value);
 	}
 
 	public String getValue() {
@@ -17,6 +30,20 @@ public class XmiValueElement extends XmiBaseElement
 
 	public void setValue(String value) {
 		this.value = value;
+	}
+
+	/**
+	 * @return the isNull
+	 */
+	public boolean isNull() {
+		return isNull;
+	}
+
+	/**
+	 * @param isNull the isNull to set
+	 */
+	public void setNull(boolean isNull) {
+		this.isNull = isNull;
 	}
 
 }
